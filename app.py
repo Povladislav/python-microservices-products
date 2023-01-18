bf7474cd1f080db645e85937ad047750e8a0ed9a
Povladislav <vladpomalejko@gmail.com>
2023-01-18 22:28:20 +0300

    commit2

---
+++ app.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    image: str
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+@dataclass
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+
+
+@app.route('/api/products')
+def index():
+    return jsonify(Product.query.all())
+
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://docker.for.ubuntu.localhost:8000/api/user')
+    return jsonify(req.json())
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True, host='0.0.0.0')
